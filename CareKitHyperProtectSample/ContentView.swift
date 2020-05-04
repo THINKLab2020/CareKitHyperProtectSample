b3a0c3e7bd3f1a9ccea0a15bb96e7474626c44bd
Elton-Desouza <elton.desouza@ca.ibm.com>
2020-05-04 19:22:52 -0400

    Initial commit for THINK Lab Sample

---
+++ CareKitHyperProtectSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CareKitHyperProtectSample
+//
+//  Created by Erik Hornberger on 5/2/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
